15ed8cfe62f42747632a32a41d8dfc45c862770c
CyrilBaah <cyril.baah@amalitech.org>
2023-05-23 14:19:22 +0000

    add link

---
+++ README.md
@@ -0,0 +1,1 @@
+## Lambda S3 | File Upload
--- upload/views.py
+++ upload/views.py
@@ -14,7 +14,7 @@ class FileUploadAPIView(APIView):
             aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
             aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
             aws_bucket_name = os.getenv('AWS_BUCKET_NAME')
-            aws_bucket_region = os.getenv('AWS_BUCKET_REGION')
+            aws_bucket_region = os.getenv('AWS_REGION')
 
             s3 = boto3.client(
                 's3',
@@ -24,9 +24,20 @@ class FileUploadAPIView(APIView):
             )
 
             try:
-                s3.upload_fileobj(file, aws_bucket_name, file.name)
-                # The file is saved to the bucket with its original name
-                return Response({'success': True, 'message': 'File uploaded successfully.'})
+                # Determine the file extension
+                file_extension = file.name.split('.')[-1]
+
+                # Generate a unique file name
+                file_name = f"{os.urandom(24).hex()}.{file_extension}"
+
+                # Upload the file to S3
+                s3.upload_fileobj(file, aws_bucket_name, file_name, ExtraArgs={"ACL": "public-read"})
+                
+
+                # Generate the S3 bucket link
+                s3_bucket_link = f"https://{aws_bucket_name}.s3.{aws_bucket_region}.amazonaws.com/{file_name}"
+
+                return Response({'success': True, 'message': 'File uploaded successfully.', 'link': s3_bucket_link})
             except Exception as e:
                 return Response({'success': False, 'message': str(e)})
         
